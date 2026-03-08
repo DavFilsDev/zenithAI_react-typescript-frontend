64bc2a087e9f9ec545984587bc8e839e6f8ff527
DavFilsDev <miharisoadavidfils@gmail.com>
2026-03-08 06:06:33 +0300

    feat(auth-types): add user authentication interfaces

---
+++ src/types/user.ts
@@ -0,0 +1,24 @@
+export interface User {
+  id: string;
+  email: string;
+  firstName?: string;
+  lastName?: string;
+  credits?: number;
+  isPremium?: boolean;
+}
+
+export interface AuthResponse {
+  access: string;
+  refresh: string;
+  user: User;
+}
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface RegisterCredentials extends LoginCredentials {
+  firstName?: string;
+  lastName?: string; 
+}
