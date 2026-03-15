508414e3e781eba2354f815f54d676738d25e12e
DavFilsDev <miharisoadavidfils@gmail.com>
2026-03-15 13:53:06 +0300

    config(theme): update tailwind config for dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+export default {
+  darkMode: 'class',
+  
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          bg: '#1a1a1a',
+          card: '#2d2d2d',
+          text: '#e5e5e5',
+        },
+      },
+    },
+  },
+  
+  plugins: [],
+}
